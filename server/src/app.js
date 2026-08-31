5d0fd598e166f01efef77d6f3b1194c92660ae1d
Sourav Kumar <souravkumar85055@gmail.com>
2026-09-01 00:33:44 +0530

    fix: allow any origin for CORS

--- server/src/app.js
+++ server/src/app.js
@@ -34,20 +34,9 @@ function buildApp() {
     })
   );
 
-  const allowedOrigins = [
-    'https://blog-website-1-ez1y.onrender.com',
-    'https://blog-website-jj8f.onrender.com',
-  ];
-
   app.use(
     cors({
-      origin(origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
+      origin: true,
       credentials: true,
     })
   );
